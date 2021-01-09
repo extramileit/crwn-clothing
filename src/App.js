cfc968d2796aa6c7fb949bf9fd662632a82d1bdb
Kurt Hartmann <extramileit@gmail.com>
2021-01-09 08:26:38 -0600

    Use CreateStructuredSelector for mapStateToProps - add beginning of Checkout component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from "./redux/user/user.actions";
-
+import {selectCurrentUser} from "./redux/user/user.selectors";
+import {createStructuredSelector} from "reselect";
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
     unsubscribeFromAuth = null;
@@ -45,6 +47,7 @@ class App extends React.Component {
                 <Switch>
                     <Route path="/" exact component={HomePage}/>
                     <Route path="/shop" component={ShopPage}/>
+                    <Route exact path="/checkout" component={CheckoutPage}/>
                     <Route exact path="/signin" render={
                         () => this.props.currentUser ? (<Redirect to='/'/>)
                             : (<SignInAndSignUpPage />)
@@ -55,8 +58,8 @@ class App extends React.Component {
     }
 
 }
-const mapStateToProps = ({user}) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
