58c911b3a6e0a411fded1e3a3ce331e255f0dec3
Kurt Hartmann <extramileit@gmail.com>
2021-01-12 07:46:18 -0600

    Move directory and shop into redux store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import {selectCollections} from "../../redux/shop/shop.selectors";
+import {createStructuredSelector} from "reselect";
+import {connect} from "react-redux";
 
+const ShopPage = ({collections}) => (
+    <div className="shop-page">
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps}/>
+            ))
+        }
+    </div>
+);
 
-class ShopPage extends React.Component {
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map( ({id, ...otherCollectionProps}) =>(
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
